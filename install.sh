0780f14d66095e854b57cba700a40ea414a24c1d
isg32 <sapangajjar101105@gmail.com>
2024-07-01 07:34:05 +0530

    bspdeb: Revamp script - fixed syntax errors; created seperate tweak files; added absolute path for script dir
    Signed-off-by: isg32 <sapangajjar101105@gmail.com>

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ oilup(){
 	"""
 }
 
+SetCurrentdir(){
+	path=$pwd
+}
 
 getPacks(){
 	sudo apt install bspwm sxhkd polybar compton rofi dunst nitrogen i3lock redshift cmus ranger
@@ -29,6 +32,7 @@ configup(){
 	
 	
 	# Installing NerdFonts
+	pip install dtrx
 	echo "Installing Nerd Fonts..."
 	cd ~/Downloads
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
@@ -40,65 +44,19 @@ configup(){
 }
 
 appendConfs(){
-	preconf_sxhkd = """
-	Super + d:                rofi
-	Super + Return:           terminal
-	Super + Shift + return:   floating terminal
-	Super + {1–9}:            go to ws
-	Super + Ctrl + {Left, Right}: go to ws
-	Super + Shift + {1–9}:    move window to ws
-	Super + f:                floating
-	Super + w:                monocle
-	Super + q:                close window
-	Super + Shift + q:        quit wm
-	Super + l:                session menu
-	Super + Esc:              reload sxhkd
-	Super + Alt + e:          reload bspwm
-	"""
+	cd $path
 	echo "Tweaking sxhkd Config..."
-	echo $preconf_sxhkd >> ~/.config/sxhkd/sxhkdrc
+	echo "$(cat tweaks_sxhkd)" >> ~/.config/sxhkd/sxhkdrc
 	
-
-	preconf_bspwm = """
-
-	sxhkd &
-	compton --backend glx --vsync opengl-swc &
-	usr/lib/xfce-polkit/xfce-polkit &
-	~/bin/polybar.launch.sh &
-	nitrogen --restore &
-	"""
 	echo "Tweaking bspwm Config..."
-	echo $preconf_bspwm >> ~/.config/bspwm/bspwmrc
+	echo "$(cat tweaks_bspwm)" >> ~/.config/bspwm/bspwmrc
 
-
-	preconf_polybar = """
-
-	#!/usr/bin/env sh
-	
-	# Terminate already running bar instances
-	killall -q polybar
-
-	# Wait until the processes have been shut down
-	while pgrep -x polybar >/dev/null; do sleep 1; done
-
-	# Launch: 'top' is the name of my Polybar
-	polybar &
-	"""
 	echo "Tweaking Polybar Config..."
-	sudo echo $preconf_polybar >> ~/bin/polybar.launch.sh
-	
-	preconf_compton = """
-	# shorter shadows
-	shadow-radius = 5;
-	shadow-offset-x = -5;
-	shadow-offset-y = -5;
-	shadow-opacity = 0.8;
-	# faster animations
-	fade-in-step = 0.07;
-	fade-out-step = 0.07;
-	"""
+	touch ~/bin/polybar.launch.sh
+	sudo echo "$(cat polybar.launch)" >> ~/bin/polybar.launch.sh
+
 	echo "Tweaking Compton Config..."
-	sudo echo $preconf_compton >> ~/.config/compton.conf
+	sudo echo "$(cat tweaks_compton)" >> ~/.config/compton.conf
 }
 
 oilup
